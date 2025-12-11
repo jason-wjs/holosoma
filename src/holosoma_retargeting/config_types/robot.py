388ca6914f2afafe44c2d60341287e09636ce505
jason-wjs <jason-w@sjtu.edu.cn>
2025-12-11 16:28:23 +0800

    fix a climbing issue

--- src/holosoma_retargeting/config_types/robot.py
+++ src/holosoma_retargeting/config_types/robot.py
@@ -288,13 +288,13 @@ class RobotConfig:
             return {"19": 0.2, "20": 0.2}  # waist yaw, waist roll
         if self.robot_type == "adam_sp":
             return {
-                "8": 1.0,  # hipRoll_Left
-                "9": 0.5,  # hipYaw_Left
-                "14": 1.0,  # hipRoll_Right
-                "15": 0.5,  # hipYaw_Right
-                "19": 0.5,  # waistRoll
-                "20": 0.5,  # waistPitch
-                "21": 0.5,  # waistYaw
+                "1": 1.0,  # hipRoll_Left
+                "2": 0.5,  # hipYaw_Left
+                "7": 1.0,  # hipRoll_Right
+                "8": 0.5,  # hipYaw_Right
+                "12": 0.5,  # waistRoll
+                "13": 0.5,  # waistPitch
+                "14": 0.5,  # waistYaw
             }
         return {}
 
@@ -310,7 +310,7 @@ class RobotConfig:
         if self.robot_type == "t1":
             return np.concatenate([np.arange(7), np.arange(11, 23)])
         if self.robot_type == "adam_sp":
-            return np.arange(29)  # All 29 joints for ADAM_SP
+            return np.arange(15)  # leg(12) an waist(3) joints for ADAM_SP
         raise ValueError(f"Invalid robot type: {self.robot_type}")
 
     NOMINAL_TRACKING_INDICES = property(
