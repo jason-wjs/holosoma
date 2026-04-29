68fac8cb0ce78b56fd0dab735ecdfec3e7807ea2
jason-wjs <jason-w@sjtu.edu.cn>
2026-04-29 11:47:59 +0800

    chore: update PARC visualization helper default

--- scripts/retargeting/vis_parc_process.sh
+++ scripts/retargeting/vis_parc_process.sh
@@ -9,20 +9,20 @@ cd "${REPO_ROOT}/src/holosoma_retargeting/holosoma_retargeting"
 ROBOT_URDF="${ROBOT_URDF:-models/g1/g1_29dof_spherehand.urdf}"
 
 ## platform_001
-python viser_player.py \
-  --robot_urdf "${ROBOT_URDF}" \
-  --object_urdf /tmp/parc_process_workspace/workspace/platform_001/multi_boxes_scaled_0.81_0.81_0.81.urdf \
-  --qpos_npz /tmp/parc_process_workspace/retargeted/platform_001_original.npz \
-  --no-assume-object-in-qpos \
-  "$@"
-
-## mid_blocks_004_dm
 # python viser_player.py \
 #   --robot_urdf "${ROBOT_URDF}" \
-#   --object_urdf /tmp/parc_process_workspace/workspace/mid_blocks_004_dm/multi_boxes_scaled_0.74_0.74_0.74.urdf \
-#   --qpos_npz /tmp/parc_process_workspace/retargeted/mid_blocks_004_dm_original.npz \
+#   --object_urdf /tmp/parc_process_workspace/workspace/platform_001/multi_boxes_scaled_0.78_0.78_0.78.urdf \
+#   --qpos_npz /tmp/parc_process_workspace/retargeted/platform_001_original.npz \
 #   --no-assume-object-in-qpos \
 #   "$@"
+
+## mid_blocks_004_dm
+python viser_player.py \
+  --robot_urdf "${ROBOT_URDF}" \
+  --object_urdf /tmp/parc_process_workspace/workspace/mid_blocks_004_dm/multi_boxes_scaled_0.78_0.78_0.78.urdf \
+  --qpos_npz /tmp/parc_process_workspace/retargeted/mid_blocks_004_dm_original.npz \
+  --no-assume-object-in-qpos \
+  "$@"
 
 ## beyond_platform_002
 # python viser_player.py \
